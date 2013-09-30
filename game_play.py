336fe49ba03322b0fe97c67a99371a724bd55594
q2nev <nwatkins@q2ebanking.com>
2013-09-30 12:53:00 -0500

    Adding 'hipsterdom.py' to separate hipster challenge function

--- game_play.py
+++ game_play.py
@@ -8,6 +8,7 @@ import logging
 import pygame.mixer as mix
 import string
 import Q2logging
+
 mix.init()
 logging.basicConfig(filename='game_play.log',logging=logging.DEBUG)
 #sys.path.insert(0,'C:/Users/nwatkins/PycharmProjects/PusherMan')
@@ -100,7 +101,7 @@ def describe(stop,extras):
                     print "__________________________________________________________"
     return stop
 
-def image_to_ascii(stop,pause_sound=False):
+def image_to_ascii(stop,pause_sound=False, guess_name=False):
     '''
     separate image_to_ascii function to have guessing game.
     image_folder: where the images are stored
@@ -108,7 +109,6 @@ def image_to_ascii(stop,pause_sound=False):
     img: string from stop.attrs
     img_txt: possible text string that would've already been generated
     '''
-    logging.debug('image_to_ascii function entered.')
     global hashes
     global ats
     image_folder = os.listdir('images/')
@@ -117,33 +117,15 @@ def image_to_ascii(stop,pause_sound=False):
     img_txt = img[:-4]+'.txt'
     logging.info(img_txt) #log image text for debugging
     play_music(stop)
-    boss_kw = str(stop.attrs["nomen"]).strip(string.whitespace)
-    #shorten this in game_play2
     if img_txt in image_folder:
         with open('images/'+img_txt) as f:
             lines = f.read()
             print "Guess ascii by pressing enter!"
-
             for l in lines.split('\t'):
-
                 while not msvcrt.kbhit():
                     time.sleep(.2)
                     break
                 print l
-
-                while msvcrt.kbhit():
-                    msvcrt.getch()
-                    play_music(stop,True)
-                    print "-----------------------------------------------"
-                    print "What's your guess?"
-                    boss_guess = raw_input(">>")
-
-                    if boss_guess == boss_kw:
-                        print "You guessed right! Here are 5 hashes and ats for your prowess!"
-                        hashes += 5
-                        ats += 5
-                    else:
-                        mix.play()
     else:
         ascii_string = ASC.image_diff('images/'+img)
         print type(ascii_string)
@@ -154,9 +136,13 @@ def image_to_ascii(stop,pause_sound=False):
         fin.close()
 
 def play_music(stop, pause_sound=False):
+    global current_sound
+
+
     try:
         sound_file = "sounds/"+str(stop.attrs["sd"]).strip(string.whitespace)
-
+        current_sound.pause()
+        current_sound = mix.Sound()
         mix.music.load(sound_file)
         logging.info('music loaded')
         if not pause_sound:
--- hipsterdom.py
+++ hipsterdom.py
@@ -4,7 +4,22 @@ import msvcrt
 import os
 import string
 import ascii as ASC
+
 import pygame.mixer as mix
+import game
+logging.basicConfig(filename='hipsterdom.log')
+hashes = 20
+ats = 20
+mix.init()
+with open("game.xml") as f:
+    xml_file = f.read()
+success, g_map = game.obj_wrapper(xml_file) #turn into python object via Q2API
+if not success:
+    logging.debug('obj_wrapper failed in main() before stops')
+    print "no object"
+    exit()
+
+stop = g_map.stop[1]
 
 def image_to_ascii(stop,pause_sound=False):
     '''
@@ -14,26 +29,32 @@ def image_to_ascii(stop,pause_sound=False):
     img: string from stop.attrs
     img_txt: possible text string that would've already been generated
     '''
-    logging.debug('image_to_ascii function entered.')
     global hashes
     global ats
+
+    logging.debug('image_to_ascii function entered.')
+
     image_folder = os.listdir('images/')
+
     logging.debug('Image folder found.')
+
     img = str(stop.attrs["im"]).strip(string.whitespace)
     img_txt = img[:-4]+'.txt'
-    logging.info(img_txt) #log image text for debugging
+    logging.debug("Image Text:",img_txt) #log image text for debugging
     play_music(stop)
     boss_kw = str(stop.attrs["nomen"]).strip(string.whitespace)
-    #shorten this in game_play2
+
+
     if img_txt in image_folder:
         with open('images/'+img_txt) as f:
             lines = f.read()
             print "Guess ascii by pressing enter!"
 
+
             for l in lines.split('\t'):
 
                 while not msvcrt.kbhit():
-                    time.sleep(.2)
+                    time.sleep(1.2)
                     break
                 print l
 
@@ -42,6 +63,7 @@ def image_to_ascii(stop,pause_sound=False):
                     play_music(stop,True)
                     print "-----------------------------------------------"
                     print "What's your guess?"
+                    print type(boss_kw)
                     boss_guess = raw_input(">>")
 
                     if boss_guess == boss_kw:
@@ -49,8 +71,9 @@ def image_to_ascii(stop,pause_sound=False):
                         hashes += 5
                         ats += 5
                     else:
-                        mix.play()
+                        mix.music.play()
     else:
+        print "Converting jpg to txt!"
         ascii_string = ASC.image_diff('images/'+img)
         print type(ascii_string)
         fin = open('images/'+img_txt,'w')
@@ -63,7 +86,7 @@ def play_music(stop, pause_sound=False):
     #sound_delay = str(stop.attrs["delay"]).strip(string.whitespace)
     try:
         sound_file = "sounds/"+str(stop.attrs["sd"]).strip(string.whitespace)
-
+        logging.info("Soundfile:", sound_file)
         mix.music.load(sound_file)
         logging.info('music loaded')
         if not pause_sound:
@@ -72,3 +95,7 @@ def play_music(stop, pause_sound=False):
             mix.music.pause()
     except:
         print "No music found"
+
+image_to_ascii(stop,True)
+print "Hashes:",hashes
+print "Ats:",ats
